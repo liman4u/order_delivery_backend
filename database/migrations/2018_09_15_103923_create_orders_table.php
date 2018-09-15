4ac7a8fd43c18f5ae6d5e0ae93c2c8790f42a6ab
Liman Adamu Labaran <limanadamu@Limans-MacBook-Pro.local>
2018-09-15 11:07:14 +0000

    Add migration file

---
+++ database/migrations/2018_09_15_103923_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('start_latitude',10,6);
+            $table->float('start_longitude',10,6);
+            $table->float('end_latitude',10,6);
+            $table->float('end_longitude',10,6);
+            $table->string('distance',75);
+            $table->string('status',25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
