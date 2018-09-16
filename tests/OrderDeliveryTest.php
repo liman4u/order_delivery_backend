446a1c46069cbaeb96334d3f13466147fcd86bb9
Liman Adamu Labaran <limanadamu@Limans-MacBook-Pro.local>
2018-09-16 11:48:39 +0000

    Add tests for can not place order

--- tests/OrderDeliveryTest.php
+++ tests/OrderDeliveryTest.php
@@ -11,23 +11,110 @@ namespace Test;
 use App\Domain\Order\Models\Order;
 use Illuminate\Http\Response;
 use Laravel\Lumen\Testing\DatabaseTransactions;
-use Log;
+
 
 class OrderDeliveryTest extends \TestCase
 {
 
     use DatabaseTransactions;
 
+    /**
+     * Test for can place order
+     */
     public function testCanPlaceOrder()
     {
         $order = factory(Order::class)->make()->toArray();
+
 
         $response = $this->post('/api/v1/order',$order);
 
 
         $response
             ->receiveJson()
-            ->seeStatusCode(Response::HTTP_CREATED);
+            ->seeStatusCode(Response::HTTP_CREATED)
+            ->seeJsonStructure(
+                    [
+                        'id',
+                        'distance',
+                        'status'
+                    ]
+
+            );
+    }
+
+    /**
+     * Test for can not place order with empty input
+     */
+    public function testCanNotPlaceOrderWithEmptyInput()
+    {
+
+        $response = $this->post('/api/v1/order',[]);
+
+
+        $response
+            ->receiveJson()
+            ->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->seeJsonStructure([
+                'errors' =>
+                    [
+                        'origin',
+                        'destination'
+                    ]
+
+            ]);
+    }
+
+
+    /**
+     * Test for can not place order with empty latitude and longitude
+     */
+    public function testCanNotPlaceOrderWithEmptyLatitudeLongitude()
+    {
+
+        $order = new Order([
+            'origin' => [],
+            'destination' => []
+         ]);
+
+        $response = $this->post('/api/v1/order',$order->toArray());
+
+        $response
+            ->receiveJson()
+            ->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->seeJsonStructure([
+                'errors' =>
+                    [
+                        'origin',
+                        'destination'
+                    ]
+
+            ]);
+    }
+
+    /**
+     * Test for can not place order with invalid latitude and longitude
+     */
+    public function testCanNotPlaceOrderWithInvalidLatitudeLongitude()
+    {
+
+        $order = new Order([
+            'origin' => ['123','123'],
+            'destination' => ['123','123']
+        ]);
+
+        $response = $this->post('/api/v1/order',$order->toArray());
+
+        $response
+            ->receiveJson()
+            ->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->seeJsonStructure([
+                'errors' =>
+                    [
+                        'origin',
+                        'destination'
+                    ]
+
+            ]);
     }
 
 }
